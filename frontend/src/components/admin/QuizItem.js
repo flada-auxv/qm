46726216b2f0bcb380afe7bd99de3efab433581a
flada-auxv <aseknock@gmail.com>
2016-11-15 02:03:34 +0900

    Card
    TODO: edit

--- frontend/src/components/admin/QuizItem.js
+++ frontend/src/components/admin/QuizItem.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Card, FontIcon, FlatButton, IconMenu, IconButton, MenuItem } from 'material-ui'
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 import sanitizeHTML from '../../sanitizer'
 
@@ -70,13 +72,34 @@ export default class QuizItem extends Component {
       )
     } else {
       element = (
-        <div>
-          <div>
-            <div onDoubleClick={this.handleContentDoubleClick} dangerouslySetInnerHTML={sanitizeHTML(`Q: ${this.state.content}`)} />
-            <div onDoubleClick={this.handleCorrectAnswerDoubleClick} dangerouslySetInnerHTML={sanitizeHTML(`A: ${this.state.correctAnswer}`)} />
+        <Card className="quiz-item" style={{ margin: "20px 0" }}>
+          <div style={{ display: "flex", padding: 16 }}>
+            <div style={{ width: "calc(100% - 50px)", display: "flex", flexDirection: "column", justifyContent: "center" }}>
+              <div style={{ display: "flex", marginBottom: 10 }}>
+                Q:<span
+                    onDoubleClick={this.handleContentDoubleClick}
+                    dangerouslySetInnerHTML={sanitizeHTML(this.state.content)}
+                    style={{ wordWrap: "break-word", paddingLeft: 5 }}
+                  />
+              </div>
+              <div style={{ display: "flex" }}>
+                A:<span
+                    onDoubleClick={this.handleCorrectAnswerDoubleClick}
+                    dangerouslySetInnerHTML={sanitizeHTML(this.state.correctAnswer)}
+                    style={{ paddingLeft: 5 }}
+                  />
+              </div>
+            </div>
+            <IconMenu
+              iconButtonElement={<IconButton><MoreVertIcon /></IconButton>}
+              anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+              targetOrigin={{horizontal: 'right', vertical: 'top'}}
+            >
+              <MenuItem primaryText="Edit" onTouchTap={() => console.log('TODO') } />
+              <MenuItem primaryText="Delete" onTouchTap={() => this.props.deleteQuizAsync(quiz.id)} />
+            </IconMenu>
           </div>
-          <button onClick={() => this.props.deleteQuizAsync(quiz.id)}>X</button>
-        </div>
+        </Card>
       )
     }
 
